63f058daf31428f4ea92373c88478a8d0645f1ac
Michał Chrobot <56775043+michalooo@users.noreply.github.com>
2024-05-15 16:18:25 +0200

    ball destruction system

---
+++ Assets/Pong/Scripts/Spawner/Ball/PongBallDestructionSystem.cs
@@ -0,0 +1,25 @@
+﻿using DeterministicLockstep;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Transforms;
+
+namespace PongGame
+{
+    [WorldSystemFilter(WorldSystemFilterFlags.ClientSimulation)]
+    [UpdateInGroup(typeof(GameStateUpdateSystemGroup))]
+    public partial class PongBallDestructionSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            var commandBuffer = new EntityCommandBuffer(Allocator.Temp);
+
+            foreach (var (transform, ballEntity) in SystemAPI.Query<LocalTransform>().WithEntityAccess())
+            {
+                if(transform.Position.x < -9f || transform.Position.x > 9f)
+                    commandBuffer.DestroyEntity(ballEntity);
+            }
+            
+            commandBuffer.Playback(EntityManager);
+        }
+    }
+}
